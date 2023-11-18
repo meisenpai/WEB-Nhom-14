9668c4d784328aff315a04a5960c2acb48c880d4
Anh Tuấn <92842026+meisenpai@users.noreply.github.com>
2023-11-18 10:11:51 +0000

    update

--- js/themVaoGioHang.js
+++ js/themVaoGioHang.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
-    $('.addToCart').click(function () {
-      var productImage = $(this).parent().parent().find('img').attr('src');
-      var productName = $(this).parent().parent().find('h5').text();
-      var productPrice = $(this).parent().parent().find('p').text();
+  $('.addToCart').click(function () {
+    var productImage = $(this).parent().parent().find('img').attr('src');
+    var productName = $(this).parent().parent().find('h5').text();
+    var productPrice = $(this).parent().parent().find('p').text();
 
-      var product = {
-        image: productImage,
-        name: productName,
-        price: productPrice,
-        quantity: 1
-      };
+    var product = {
+      image: productImage,
+      name: productName,
+      price: productPrice,
+      quantity: 1
+    };
 
-      var cart = JSON.parse(localStorage.getItem('cart')) || [];
-      cart.push(product);
-      localStorage.setItem('cart', JSON.stringify(cart));
-      $('#message').text('Thêm sản phẩm vào giỏ hàng thành công!');
-      $('#message').removeClass('hidden');
-      setTimeout(function () {
-        $('#message').addClass('hidden');
-      }, 2000);
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    var existingProduct = cart.find(function (item) {
+      return item.name === product.name;
     });
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      cart.push(product);
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    $('#message').text('Thêm vào giỏ hàng thành công!');
+    $('#message').removeClass('hidden');
+    setTimeout(function () {
+      $('#message').addClass('hidden');
+    }, 2000);
   });
+});
